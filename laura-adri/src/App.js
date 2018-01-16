fec6651f54f3185902e64056b1c872816b638d8e
MarkyMark <mgelabert3@gmail.com>
2018-01-16 17:59:42 +0100

    landingPage

--- laura-adri/src/App.js
+++ laura-adri/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './animate.css'
 
 
 import Landing from './components/landing'
@@ -8,7 +9,15 @@ import Landing from './components/landing'
 class App extends Component {
   render() {
     return (
-          <Landing/>
+      <div>
+        <Landing/>
+        <div className="image__wrapper">
+          <h1 className="text__save animated fadeIn">Save The Date  7-7-2018</h1>
+          <img className="saveTheDate animated fadeIn" src={require ("./assets/save.jpg")}/>
+        </div>
+
+      </div>
+
     );
   }
 }
--- laura-adri/src/components/landing.js
+++ laura-adri/src/components/landing.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 import '../App.css';
+import '../animate.css';
 
 export default class Landing extends Component {
   render() {
     return (
       <div className="topMenu__wrapper">
-        <div className="topMenu__logo">
-          <h1>LOGO GOES HERE</h1>
+        <div className="topMenu__logo animated fadeIn">
+          <h1 className="adrilaura">LaurAdri</h1>
         </div>
-        <ul className="topMenu">
-          <li>Informacio Boda</li>
+        <ul className="topMenu animated fadeIn">
+          <li>Informació Boda</li>
           <li>Hotels</li>
           <li>Vestimenta</li>
           <li>RSVP</li>
