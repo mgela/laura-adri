e004014e0b1f23bb873a6224291bff946a0fa9a0
MarkyMark <mgelabert3@gmail.com>
2018-01-28 18:56:05 +0100

    icons and menu component

--- laura-adri/src/App.js
+++ laura-adri/src/App.js
@@ -10,7 +10,21 @@ class App extends Component {
   render() {
     return (
       <div className="appContainer">
-        <Landing/>
+        {/* <Landing/> */}
+        <div className="topMenu__wrapper">
+          <div className="topMenu__logo">
+            <h1 className="topMenu__title">Laura i Adri</h1>
+          </div>
+          <ul className="topMenu animated fadeInRight">
+            <li>La Boda</li>
+            <li>Hotels</li>
+            <li>Vestimenta</li>
+            <li>RSVP</li>
+            <li>Album</li>
+            <li>Video</li>
+            <li>Regal</li>
+          </ul>
+        </div>
         <MenuComponent/>
         {/* <div className="image__wrapper">
           <h1 className="text__save animated fadeIn">Save The Date  7-7-2018</h1>
--- laura-adri/src/components/menuComponent.js
+++ laura-adri/src/components/menuComponent.js
@@ -8,19 +8,24 @@ export default class MenuComponent extends Component {
     return (
       <div className="hotels__wrapper">
         <div className='boda'>
-
+          <img className="bodaIcon" src={require ("../assets/ring.png")}/>
+          <h2>La Boda</h2>
         </div>
         <div className='hotels'>
-
+          <img className="bodaIcon" src={require ("../assets/hotel.png")}/>
+          <h2>Hotels</h2>
         </div>
         <div className='dresscode'>
-
+          <img className="bodaIcon" src={require ("../assets/dresscode.png")}/>
+          <h2>Dress Code</h2>
         </div>
         <div className='rsvp'>
-
+          <img className="bodaIcon" src={require ("../assets/rsvp.png")}/>
+          <h2>Vindràs</h2>
         </div>
         <div className='regal'>
-
+          <img className="bodaIcon" src={require ("../assets/regals.png")}/>
+          <h2>No clicar!</h2>
         </div>
       </div>
     );
